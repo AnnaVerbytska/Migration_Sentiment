db12dde2e851090f541178854f755512510802ec
Anna Verbytska <verbytska.anna@outlook.com>
2025-08-31 15:14:48 +0200

    Debug 10 for Dash

--- src/dashboard.py
+++ src/dashboard.py
@@ -14,7 +14,8 @@ from .visualisations import (
     plot_stance_heatmap_by_subreddit,
     plot_target_group_proportions,
     plot_stance_and_intensity_summary,
-    plot_most_polarized_targets
+    plot_polarization_heatmap
+    
     
 )
 # --- 1. App Setup ---
@@ -57,9 +58,9 @@ app.layout = html.Div([
     html.H3("How do the stances and intensity levels for the target groups compare between the two subreddits?", style={'textAlign': 'center', 'fontFamily': 'Arial'}),
     dcc.Graph(figure=plot_stance_and_intensity_summary(df, category_col='target_label', subreddit_col='subreddit')),
 
-    # Most Polarized Targets Across Subreddits
-    html.H3("Which targets are the most polarized, and what is the proportion of supportive versus critical stances for each across subreddits?", style={'textAlign': 'center', 'fontFamily': 'Arial'}),
-    dcc.Graph(figure=plot_most_polarized_targets(df, subreddit_col='subreddit', target_col='target', stance_col='stance'))
+    # Stance Intensity Distribution by Target Group
+    html.H3("How does the proportion of supportive versus critical stances vary in each target group?", style={'textAlign': 'center', 'fontFamily': 'Arial'}),
+    dcc.Graph(figure=plot_polarization_heatmap(df, category_col='target_label'))
     
 
     ])
