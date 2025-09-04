6a73fd18e706a04439200eea5143777c8a1c02b6
Anna Verbytska <verbytska.anna@outlook.com>
2025-09-04 12:08:08 +0200

    Bug fix

--- src/dashboard.py
+++ src/dashboard.py
@@ -9,12 +9,7 @@ import base64
 import matplotlib.pyplot as plt
 
 # --- Use a relative import for local modules ---
-from .visualisations import (
-    plot_top_targets_by_subreddit,
-    plot_stance_heatmap_by_subreddit,
-    plot_target_group_proportions,
-    plot_stance_and_intensity_summary
-)
+from .visualisations import (plot_stance_and_intensity_summary)
 # --- 1. App Setup ---
 
 # --- Create a robust, absolute path to the data file ---
@@ -36,24 +31,9 @@ server = app.server
 app.layout = html.Div([
     html.H1("Migration as a Strategic Narrative in War", style={'textAlign': 'center', 'fontFamily': 'Arial'}),
 
-    # Subheading for Target and Stance Analysis
-    html.H2("Stance and Target Analysis", style={'textAlign': 'center', 'fontFamily': 'Arial', 'marginTop': '50px', 'marginBottom': '20px'}),
-    
-    # Target Distribution Graph
-    html.H3("Which migration-related targets are most discussed in the contexts of Israel-Gaza and Russia-Ukraine wars?", style={'textAlign': 'center', 'fontFamily': 'Arial'}),
-    dcc.Graph(figure=plot_top_targets_by_subreddit(df, subreddit_col='subreddit', target_col='target')),
-
-    # Polarization Index by Subreddit
-    html.H3("To what extent is discourse polarized in each subreddit?", style={'textAlign': 'center', 'fontFamily': 'Arial'}),
-    dcc.Graph(figure=plot_stance_heatmap_by_subreddit(df, subreddit_col='subreddit', stance_col='stance', intensity_col='confidence_intensity')),
-
-     # Proportion of Target Group Mentions
-    html.H3("What is the proportional breakdown of mentions for each migration-related target group?", style={'textAlign': 'center', 'fontFamily': 'Arial'}),
-    dcc.Graph(figure=plot_target_group_proportions(df, target_label_col='target_label')),
-
     # Stance & Intensity Across Target Groups and Subreddits
     html.H3("How do the stances and intensity levels for the target groups compare between the two subreddits?", style={'textAlign': 'center', 'fontFamily': 'Arial'}),
-    dcc.Graph(figure=plot_stance_and_intensity_summary(df, category_col='target_label', subreddit_col='subreddit')),
+    dcc.Graph(figure=plot_stance_and_intensity_summary(df, category_col='target_label', subreddit_col='subreddit'))
 
     ])
 
